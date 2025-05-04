ae671716c8a0066971aa8aa76b922829918b4eeb
Ahmed Ashraf Elsayed <125831172+ahmedafify45@users.noreply.github.com>
2025-05-04 21:03:08 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+// next.config.js or next.config.ts
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
-  },
-  build: {
-    env: {
-      DISABLE_ESLINT_PLUGIN: "true",
-    },
   },
 };
 
