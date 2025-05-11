49db9184314b0735a6aec0ff2e877b0bda16f5ad
Your Name <your.email@example.com>
2025-05-11 15:09:12 +0300

    initial commit

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,7 +5,7 @@ const Loading = () => {
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-black backdrop-blur-sm z-50">
       <Image
         className="mt-4 text-2xl font-bold text-primary text-center"
-        src="/images/logo.png"
+        src="/images/via_SVG.svg"
         alt="logo"
         width={100}
         height={100}
